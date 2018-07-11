9763b81380fa969c23dd71946d83abb8008fcaaf
OlafAltwirren <olaf.altwirren@airflowrental.de>
2018-07-11 11:05:51 +0200

    Fixed refactoring errors.

--- network/boot/82_hostname.lua
+++ network/boot/82_hostname.lua
@@ -18,8 +18,9 @@ event.listen("network_ready", function()
         logger.log("Loading INP configuration...")
         local hostnameConfig = fileconfig.loadConfig("inp.json", defaultHostnameConfig)
 
-        -- process config
+        logger.log("INP->"..hostnameConfig)
 
+        -- process config
         for interfaceUUID in pairs(hostnameConfig) do
             logger.log("Binding hostnames for interface "..interfaceUUID)
             for domainName in pairs(hostnameConfig[interfaceUUID]) do
